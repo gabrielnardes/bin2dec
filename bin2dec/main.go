8708003a0ad94ed5c7a1a92b839908d1e12f92d7
Gabriel Nardes Giampietro <gabrielnardeslobao@gmail.com>
2024-02-10 23:00:48 -0300

    Init repo

---
+++ bin2dec/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var i string
+
+    fmt.Println("Input")
+    fmt.Scan(&i)
+    fmt.Println("Output: ", i)
+}
+
---
+++ bin2dec/main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "testing"
+)
+
+func TestTheMaxiumOf8DigitsInput(t *testing.T) {
+
+}
+
+func TestUserNotificationWhenInputIsDifferentOf0Or1(t *testing.T) {
+
+}
+
+func TestIfTheResultIsInASingleOuput(t *testing.T) {
+
+}
